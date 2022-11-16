573c578db129c85343fd84778cdaa186531ce63c
12Me21 <12Me21.mc@gmail.com>
2022-11-16 06:51:35 -0500

    eee

---
+++ background.js
@@ -0,0 +1,44 @@
+console.log('running')
+
+function make_filter(event, field, process) {
+	browser.webRequest[event].addListener(
+		({[field]:headers})=>({[field]:process(headers)}),
+		{urls: ["https://cohost.org/api/v1//*"]},
+		["blocking", field],
+	)
+}
+
+make_filter(
+	'onBeforeSendHeaders', 'requestHeaders',
+	headers=>{
+		console.log('got headers')
+		return headers.filter(h=>{
+			if (h.name.toLowerCase()=='cookie')
+				return false
+			if (h.name=='x_12_cookie')
+				h.name = 'cookie'
+			return true
+		})
+	},
+)
+
+make_filter(
+	'onHeadersReceived', 'responseHeaders',
+	headers=>{
+		let h2 = headers.map(h=>{
+			if (h.name.toLowerCase()=='content-language') {
+				let c = headers.find(x=>x.name.toLowerCase()=='set-cookie')
+				if (c)
+					h.value = c.value
+			}
+			return h
+		}).concat([
+			{name:'access-control-allow-origin', value:"*"},
+			{name:'access-control-allow-methods', value:"POST, GET, OPTIONS"},
+			{name:'access-control-allow-headers', value:"x_12_cookie"},
+			{name:'access-control-allow-credentials', value:"true"},
+		])
+		console.log('got response', h2)
+		return h2
+	},
+)
---
+++ hash.js
@@ -0,0 +1,57 @@
+"use strict"
+async function get_hash(password, salt) {
+	salt = atob(salt.replace(/[^A-Za-z0-9+/]/g, 'A'))
+	salt = Uint8Array.from(salt, x=>x.charCodeAt()).buffer
+	
+	password = new TextEncoder().encode(password)
+	
+	// todo: is therea shorter way to do this? json web key format?
+	const def = {name: 'PBKDF2', hash: 'SHA-384', iterations: 2e5, salt}
+	let key
+	key = await crypto.subtle.importKey(
+		'raw', password, def, false, ['deriveKey']
+	)
+	key = await crypto.subtle.deriveKey(def, key, {
+		name: 'HMAC',
+		hash: 'SHA-384',
+	}, true, ['sign'])
+	key = await crypto.subtle.exportKey('raw', key)
+	
+	return btoa(String.fromCharCode(...new Uint8Array(key)))
+}
+
+async function get_salt(email) {
+	let url = new URL('https://cohost.org/api/v1//login/salt')
+	url.searchParams.set('email', email)
+	let resp = await fetch(url)
+	let data = await resp.json()
+	return data.salt
+}
+
+async function log_in(email, hash) {
+	let url = new URL('https://cohost.org/api/v1//login')
+	let body = new URLSearchParams([['email', email],['clientHash', hash]])
+	let resp = await fetch(url, {method:'POST',body,mode:'cors',headers:{'x-idk':'3'}})
+	let cookie = /connect.sid=(.*?);/.exec(resp.headers.get('content-language'))?.[1]
+	let data = await resp.json()
+	return [data, cookie]
+}
+
+async function go(email, password) {
+	let salt = await get_salt(email)
+	let hash = await get_hash(password, salt)
+	let data = await log_in(email, hash)
+	return data
+}
+
+async function api_request(endpoint, cookie, data) {
+	let config = {
+		headers: {x_12_cookie: 'connect.sid='+cookie},
+	}
+	if (data!==undefined) {
+		config.method = 'POST'
+		config.body = new Blob([JSON.stringify(data)], {type: "application/json;charset=UTF-8"})
+	}
+	let resp = await fetch(`https://cohost.org/api/v1//trpc/${endpoint}`, config)
+	return await resp.json()
+}
