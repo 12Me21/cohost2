174619b6d43644e50b13ac3ff548a6317915ebeb
12Me21 <12Me21.mc@gmail.com>
2022-11-25 05:19:00 -0500

    oops forgot comment

---
+++ type/comment.js
@@ -0,0 +1,30 @@
+class Comment {
+	static fallback(id) {
+		return {
+			canEdit: 'not-allowed',
+			canInteract: 'not-allowed',
+			canHide: 'not-allowed',
+			comment: {
+				body: "",
+				children: [],
+				commentId: id,
+				deleted: false,
+				postId: 0,
+				postedAtISO: '2000-01-01T00:00:00Z',
+				inReplyTo: "00000000-0000-0000-0000-000000000000",
+				hasCohostPlus: false,
+				hidden: false,
+			},
+			poster: {
+				projectId: 0
+			},
+		}
+	}
+	
+	constructor(data=null, id=0) {
+		if (!data)
+			data = Comment.fallback(id)
+		Object.setPrototypeOf(data, Comment.prototype)
+		this.postedAt = new Date(this.postedAtISO)
+	}
+}
